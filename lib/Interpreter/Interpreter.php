4a7efe84000164f9db31c5121f7531b5a3eb40f1
Raul <raul@nextcloud.com>
2022-06-19 23:43:21 +0200

    Use nextcloud temporary directory for PHP generation

--- lib/Interpreter/Interpreter.php
+++ lib/Interpreter/Interpreter.php
@@ -3,12 +3,19 @@ namespace OCA\FilesScripts\Interpreter;
 
 use Lua;
 use OCA\FilesScripts\Db\Script;
+use OCP\ITempManager;
+use raudius\phpdf\Phpdf;
 
 class Interpreter {
 	private FunctionProvider $functionProvider;
+	private ITempManager $tempManager;
 
-	public function __construct(FunctionProvider $functionProvider) {
+	public function __construct(
+		FunctionProvider $functionProvider,
+		ITempManager $tempManager
+	) {
 		$this->functionProvider = $functionProvider;
+		$this->tempManager = $tempManager;
 	}
 
 	/**
@@ -19,6 +26,7 @@ class Interpreter {
 
 		$oldVal = ignore_user_abort(true);
 
+		Phpdf::setTempDirectory($this->tempManager->getTempBaseDir());
 		$lua->eval($script->getProgram());
 
 		ignore_user_abort($oldVal);

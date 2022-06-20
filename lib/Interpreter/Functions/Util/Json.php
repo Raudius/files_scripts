aff5f88d7646b1453f9a558a44c3c4d51d01c833
Raul <raul@nextcloud.com>
2022-06-20 10:43:45 +0200

    Expand json function.

--- lib/Interpreter/Functions/Util/Json.php
+++ lib/Interpreter/Functions/Util/Json.php
@@ -4,13 +4,23 @@ namespace OCA\FilesScripts\Interpreter\Functions\Util;
 use OCA\FilesScripts\Interpreter\RegistrableFunction;
 
 /**
- * `json(Table table): String`
+ * `json(Table|string input): String|Table|nil`
  *
- * Returns the json encoding of the given table. If the encoding fails, `nil` is returned.
+ * If the input is a string, returns a Table of the JSON represented in the string.
+ * If the input is a table, returns the JSON representation of that object.
+ * If encoding/decoding fails, `nil` is returned.
  */
 class Json extends RegistrableFunction {
-	public function run(array $table = []): ?string {
-		$table = $this->normaliseArray($table);
-		return json_encode($table) ?: null;
+	public function run($input = []) {
+		if (is_string($input)) {
+			return json_decode($input) ?: null;
+		}
+
+		if (is_array($input)) {
+			$input = $this->normaliseArray($input);
+			return json_encode($input) ?: null;
+		}
+
+		return null;
 	}
 }

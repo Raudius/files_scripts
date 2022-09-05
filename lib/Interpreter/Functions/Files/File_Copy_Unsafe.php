bcb6ac0f046a77dc89e51d99dda27aa879261da6
Raul <r.ferreira.fuentes@gmail.com>
2022-09-05 18:25:55 +0200

    Add "unsafe" copy/move functions

---
+++ lib/Interpreter/Functions/Files/File_Copy_Unsafe.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OCA\FilesScripts\Interpreter\Functions\Files;
+
+use OCA\FilesScripts\Interpreter\RegistrableFunction;
+
+/**
+ * `file_copy_unsafe(Node file, String folder_path, [String name]=nil): Bool`
+ *
+ * Unsafe version of `copy_file`.
+ * This function expects an absolute path from the server root (not from the users home folder). This means that files can be copied to locations which the user running the action does not have access to.
+ * This function performs no validation on the given path and does not check for file overwrites (overwrite handling is left up to the Nextcloud server implementation).
+ *
+ * ⚠️ Use of this function is strongly discouraged as it offers no safeguards against data-loss and carries potential security concerns.
+ *
+ * ```lua
+ * local file = get_input_files()[1]
+ * file_copy_unsafe(file, "alice/files/inbox", "message.txt")
+ * ```
+ */
+class File_Copy_Unsafe extends RegistrableFunction {
+	public function run(
+		$file = null,
+		$folderPath = null,
+		$name = null
+	) {
+		$fileNode = $this->getFile($this->getPath($file));
+		if (!$fileNode) {
+			return false;
+		}
+
+		$name = $name ?: $fileNode->getName();
+		$path = $folderPath . '/' . $name;
+
+		try {
+			$newFile = $fileNode->copy($path);
+			return $this->getNodeData($newFile);
+		} catch (\Exception $exception) {
+			return null;
+		}
+	}
+}
---
+++ lib/Interpreter/Functions/Files/File_Move_Unsafe.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OCA\FilesScripts\Interpreter\Functions\Files;
+
+use OCA\FilesScripts\Interpreter\RegistrableFunction;
+use Throwable;
+
+/**
+ * `file_move_unsafe(Node file, [String folder = nil], [String new_name = nil]): Node|null`
+ *
+ * Unsafe version of `file_move`.
+ * This function expects an absolute path from the server root (not from the users home folder). This means that files can be copied to locations which the user running the action does not have access to.
+ * This function performs no validation on the given path and does not check for file overwrites (overwrite handling is left up to the Nextcloud server implementation).
+ *
+ * Note: If
+ *
+ * ⚠️ Use of this function is strongly discouraged as it offers no safeguards against data-loss and carries potential security concerns.
+ *
+ * ```lua
+ * local file = get_input_files()[1]
+ * file_move_unsafe(file, "alice/files/inbox", "message.txt")
+ * ```
+ */
+class File_Move_Unsafe extends RegistrableFunction {
+
+	public function run(
+		$file = null,
+		$folderPath = null,
+		$newName = null
+	): ?array {
+		$fileNode = $this->getFile($this->getPath($file));
+		if (!$fileNode) {
+			return null;
+		}
+
+		$newName = $newName ?: $fileNode->getName();
+		$destination = $folderPath . '/' . $newName;
+
+		try {
+			$newFileNode = $fileNode->move($destination);
+			return $this->getNodeData($newFileNode);
+		} catch (Throwable $e) {
+			return null;
+		}
+	}
+}

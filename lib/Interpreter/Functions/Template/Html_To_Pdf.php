13be41a1a03c44f7ddb55201d4e33e0f08f406f0
Raul Ferreira <r.ferreira.fuentes@gmail.com>
2023-12-17 16:37:51 +0100

    Update `html_to_pdf` description
    Signed-off-by: Raul Ferreira <r.ferreira.fuentes@gmail.com>

--- lib/Interpreter/Functions/Template/Html_To_Pdf.php
+++ lib/Interpreter/Functions/Template/Html_To_Pdf.php
@@ -10,7 +10,7 @@ use OCA\FilesScripts\Interpreter\RegistrableFunction;
  *
  * This function has been removed. The dependency that makes this function work is excessively large, and it is unnecessary to package it with the `file_scripts` app.
  *
- * You can continue using the function by manually installing the FilesScriptsHtml2Pdf app.
+ * You can continue using the function by manually installing the [`files_scripts_deprecated`](https://github.com/Raudius/files_scripts_deprecated) app, which bundles all the removed functions.
  */
 class Html_To_Pdf extends RegistrableFunction {
 

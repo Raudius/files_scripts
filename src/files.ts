b956f1496cc8aae93553b95e7cfd61c7a5692fcf
Raul Ferreira Fuentes <r.ferreira.fuentes@gmail.com>
2026-06-11 22:47:21 +0200

    cleanup: remove legacy integrations with files app
    Signed-off-by: Raul Ferreira Fuentes <r.ferreira.fuentes@gmail.com>

--- src/files.ts
+++ src/files.ts
@@ -4,26 +4,16 @@ import {Script, scriptAllowedForNodes} from "./types/script";
 import FileCog from '@mdi/svg/svg/file-cog.svg';
 import {emit} from "@nextcloud/event-bus"
 
-
 export function reloadDirectory(node: Node){
 	node && emit("files:node:updated", node)
-
-	// Legacy reload for public shares
-	const fileList = OCA?.Sharing?.PublicApp?.fileList
-	if (!fileList) {
-		return
-	}
-	fileList.changeDirectory(fileList.getCurrentDirectory(), true, true)
 }
 
 export type HandlerFunc = (files: ActionContext) => void;
-
 
 function buildActionObject(myHandler: HandlerFunc, script: Script|null = null): IFileAction {
 	const displayName = script ? script.title : t('files_scripts', 'More actions')
 	const id = 'files_scripts_action' + (script ? script.id : "")
 	const order = 1000 + (script ? 0 : 1)
-
 
 	return {
 		id: id,
@@ -45,35 +35,8 @@ function buildActionObject(myHandler: HandlerFunc, script: Script|null = null): 
 	} as IFileAction
 }
 
-function nodeFromLegacy(file: any): Node {
-	return {
-		get fileid(): number | undefined {
-			return file.id
-		},
-		get basename(): string {
-			return file.name
-		},
-		get path(): string {
-			return file.path
-		}
-	} as Node
-}
-
-function registerPublicSharingFileAction(action: IFileAction, script: Script) {
-	if (!OCA.Sharing || !OCA.Sharing.PublicApp || !OCA.Sharing.PublicApp.fileList) {
-		return
-	}
-	// TODO fix register public file sharing
-}
-
 export function registerMenuOption(handler: HandlerFunc, script=null) {
 	const actionObject = buildActionObject(handler, script)
 	registerFileAction(actionObject)
-
-	// FIXME: 	Public shares are still using the legacy file viewer, if it ever updates to the Vue version
-	//  		we should update this code.
-	setTimeout(() => {
-		registerPublicSharingFileAction(actionObject, script)
-	}, 2000)
 }
 
